14c16a187364dd2ee4a9eb44f4adc7252d0c174d
angel <angel@local>
2026-08-06 09:56:23 -0500

    chore(results): update results 76 files

---
+++ results/src-repro-20260806T035656Z/examples/gguf-hash/CMakeFiles/xxhash.dir/deps/xxhash/xxhash.c.o.d
@@ -0,0 +1,73 @@
+examples/gguf-hash/CMakeFiles/xxhash.dir/deps/xxhash/xxhash.c.o: \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/examples/gguf-hash/deps/xxhash/xxhash.c \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/examples/gguf-hash/deps/xxhash/xxhash.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stddef.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_header_macro.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_ptrdiff_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_size_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_wchar_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_null.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_max_align_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_offsetof.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdint.h \
+  /usr/include/stdint.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdalign.h \
+  /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+  /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+  /usr/include/x86_64-linux-gnu/sys/types.h \
+  /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+  /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+  /usr/include/x86_64-linux-gnu/bits/endianness.h \
+  /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+  /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+  /usr/include/x86_64-linux-gnu/sys/select.h \
+  /usr/include/x86_64-linux-gnu/bits/select.h \
+  /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+  /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+  /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+  /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+  /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+  /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+  /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h \
+  /usr/include/alloca.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+  /usr/include/string.h \
+  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+  /usr/include/strings.h \
+  /opt/intel/oneapi/compiler/2026.0/bin/compiler/../../opt/compiler/include/limits.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/limits.h \
+  /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+  /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+  /usr/include/linux/limits.h \
+  /usr/include/x86_64-linux-gnu/bits/pthread_stack_min-dynamic.h \
+  /usr/include/x86_64-linux-gnu/bits/pthread_stack_min.h \
+  /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/emmintrin.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/xmmintrin.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/mmintrin.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/mm_malloc.h
