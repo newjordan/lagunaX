acada0dcdb0debd6688da027a37fb7d3f758452d
angel <angel@local>
2026-08-06 09:56:23 -0500

    test(results): cover results 56 files

---
+++ results/src-repro-20260806T035656Z/tests/CMakeFiles/test-c.dir/test-c.c.o.d
@@ -0,0 +1,48 @@
+tests/CMakeFiles/test-c.dir/test-c.c.o: \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/tests/test-c.c \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/src/../include/llama.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdbool.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stddef.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_header_macro.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_ptrdiff_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_size_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_wchar_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_null.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_max_align_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_offsetof.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdint.h \
+  /usr/include/stdint.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-least.h /usr/include/stdio.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdarg.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stdarg___gnuc_va_list.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-cpu.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-backend.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-alloc.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-opt.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/gguf.h
---
+++ results/src-repro-20260806T035656Z/tests/CMakeFiles/test-model-load-cancel.dir/test-model-load-cancel.cpp.o.d
@@ -0,0 +1,85 @@
+tests/CMakeFiles/test-model-load-cancel.dir/test-model-load-cancel.cpp.o: \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/tests/test-model-load-cancel.cpp \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/src/../include/llama.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdbool.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stddef.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_header_macro.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_ptrdiff_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_size_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_wchar_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_null.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_nullptr_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_max_align_t.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stddef_offsetof.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdint.h \
+  /usr/include/stdint.h \
+  /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+  /usr/include/features.h /usr/include/features-time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+  /usr/include/x86_64-linux-gnu/bits/timesize.h \
+  /usr/include/stdc-predef.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+  /usr/include/x86_64-linux-gnu/bits/long-double.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+  /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+  /usr/include/x86_64-linux-gnu/bits/types.h \
+  /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+  /usr/include/x86_64-linux-gnu/bits/time64.h \
+  /usr/include/x86_64-linux-gnu/bits/wchar.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+  /usr/include/x86_64-linux-gnu/bits/stdint-least.h /usr/include/stdio.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/stdarg.h \
+  /opt/intel/oneapi/compiler/2026.0/lib/clang/22/include/__stdarg___gnuc_va_list.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+  /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn.h \
+  /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+  /usr/include/x86_64-linux-gnu/bits/stdio.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-cpu.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-backend.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-alloc.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/ggml-opt.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/ggml/src/../include/gguf.h \
+  /home/frosty40/turbo/worktrees/treebeard-base-control-latest/tests/get-model.h \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/c++/13/cstdlib \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/c++/13/pstl/pstl_config.h \
+  /opt/intel/oneapi/tbb/2023.0/env/../include/tbb/tbb.h \
+  /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+  /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+  /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+  /usr/include/x86_64-linux-gnu/sys/types.h \
+  /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+  /usr/include/endian.h /usr/include/x86_64-linux-gnu/bits/endian.h \
+  /usr/include/x86_64-linux-gnu/bits/endianness.h \
+  /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+  /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+  /usr/include/x86_64-linux-gnu/sys/select.h \
+  /usr/include/x86_64-linux-gnu/bits/select.h \
+  /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+  /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+  /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+  /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+  /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+  /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+  /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+  /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h \
+  /usr/include/alloca.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+  /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../../include/c++/13/bits/std_abs.h
